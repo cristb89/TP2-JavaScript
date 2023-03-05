a1fde187dd00d0ada249a4eada51ca2cc4c19aa8
Cristian Brandan <cristian_brandan@hotmail.com>
2023-03-04 23:28:20 -0300

    Ejercicio 03 - Resuelto

---
+++ js/ejercicio03.js
@@ -0,0 +1,20 @@
+/*
+
+3- Realiza un script que pida cadenas de texto hasta que se pulse “cancelar”. Al
+salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un
+guión -.
+Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+*/
+
+let confirmacion = true;
+let texto = "";
+let cadena = "";
+
+while (confirmacion !== false) {
+    texto = prompt("Ingrese texto");
+    confirmacion = confirm("Desea continuar");
+    cadena += texto + "-";
+}
+
+document.write(cadena);
